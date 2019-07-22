c998cdf024f1249f92e1e547925abf4a94fbcabc
Lauriane Haydari <lhaydari@MacBook-Pro-de-Lauriane.local>
2019-07-22 17:49:45 +0200

    Add tests 59%

--- CountOnMeTests/CountOnMeTests.swift
+++ CountOnMeTests/CountOnMeTests.swift
@@ -1,39 +1,38 @@
 //
-//  ViewModel.swift
-//  CountOnMe
+//  CountOnMeTests.swift
+//  CountOnMeTests
 //
-//  Created by Lauriane Haydari on 17/07/2019.
-//  Copyright © 2019 Lauriane Haydari. All rights reserved.
+//  Created by Lauriane Haydari on 22/07/2019.
+//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
 //
 
 @testable import CountOnMe
+
 import XCTest
 
 final class ViewModelTests: XCTestCase {
     
     var viewModel: ViewModel!
-    
     override func setUp() {
+        
         super.setUp()
         viewModel = ViewModel()
     }
-
+    
     func testGivenAViewModelWhenViewDidLoadThenDisplayedTextIsCorrctlyReturned() {
         let expectation = self.expectation(description: "Returned text")
-
         viewModel.displayedText = { text in
+            
             XCTAssertEqual(text, "1 + 2 = 3")
+            
             expectation.fulfill()
         }
-
         viewModel.viewDidLoad()
-
         waitForExpectations(timeout: 1.0, handler: nil)
     }
-
+    
     func testGivenAViewModelWhenDidPressNumberButtonThenDisplayedTextIsCorrctlyReturned() {
         let expectation = self.expectation(description: "Returned text")
-        
         var counter = 0
         viewModel.displayedText = { text in
             if counter == 1 {
@@ -44,15 +43,12 @@ final class ViewModelTests: XCTestCase {
         }
         
         viewModel.viewDidLoad()
-
         viewModel.didPressNumberButton(with: "2")
-        
         waitForExpectations(timeout: 1.0, handler: nil)
     }
     
     func testGivenAViewModelWhenDidSelectOperatorIsMultiplyThenDisplayedTextIsCorrctlyReturned() {
         let expectation = self.expectation(description: "Returned text")
-        
         var counter = 0
         viewModel.displayedText = { text in
             if counter == 1 {
@@ -63,16 +59,16 @@ final class ViewModelTests: XCTestCase {
         }
         
         viewModel.viewDidLoad()
-        
         viewModel.didSelectOperator(at: 0)
-        
         waitForExpectations(timeout: 1.0, handler: nil)
+        
     }
     
     func testGivenAViewModelWhenDidSelectOperatorIsEqualThenDisplayedTextIsCorrctlyReturned() {
         let expectation = self.expectation(description: "Returned text")
         
         var counter = 0
+        
         viewModel.displayedText = { text in
             if counter == 1 {
                 XCTAssertEqual(text, "=")
@@ -82,24 +78,56 @@ final class ViewModelTests: XCTestCase {
         }
         
         viewModel.viewDidLoad()
-        
         viewModel.didSelectOperator(at: 4)
         
         waitForExpectations(timeout: 1.0, handler: nil)
     }
     
-    func testGivenOneAndTwoWhenDidPressSelectAdditionThenResultIs3() {
-//        let viewModel = ViewModel()
-//        let expectation = self.expectation(description: "Returned text")
+//    func testGivenAViewModelWhenDidPressReturnButtonThenDisplayedTextIsCorrctlyReturned() {
+//        viewModel.didPressNumberButton(with: "2")
+//        viewModel.didSelectOperator(at: 1)
 //
 //        var counter = 0
+//        
 //        viewModel.displayedText = { text in
-//            if counter == 5 {
-//                XCTAssertEqual(text, "1+2=3")
+//            if counter == 1 {
+//                XCTAssertEqual(text, "2")
 //                expectation.fulfill()
 //            }
-//             counter += 1
+//            counter += 1
 //        }
+//
+//
+//        viewModel.didPressReturnButton()
+//
+//        XCTAssertEqual(viewModel.result, 1)
+//
+//    }
+
+    func testGivenOneAndTwoWhenDidPressSelectAdditionThenResultIs3() {
+        
+        //        let viewModel = ViewModel()
+        
+        //        let expectation = self.expectation(description: "Returned text")
+        
+        //
+        
+        //        var counter = 0
+        
+        //        viewModel.displayedText = { text in
+        
+        //            if counter == 5 {
+        
+        //                XCTAssertEqual(text, "1+2=3")
+        
+        //                expectation.fulfill()
+        
+        //            }
+        
+        //             counter += 1
+        
+        //        }
+        
         viewModel.didPressNumberButton(with: "1")
         viewModel.didSelectOperator(at: 0)
         viewModel.didPressNumberButton(with: "2")
@@ -107,12 +135,10 @@ final class ViewModelTests: XCTestCase {
         viewModel.didPressEqualButton()
         
         XCTAssertEqual(viewModel.result, 3)
-//        waitForExpectations(timeout: 1.0, handler: nil)
-        
+        //        waitForExpectations(timeout: 1.0, handler: nil)
     }
     
     func testGivenTwoAndOneWhenDidPressSelectSubstractionThenResultIs1() {
-        
         viewModel.didPressNumberButton(with: "2")
         viewModel.didSelectOperator(at: 1)
         viewModel.didPressNumberButton(with: "1")
@@ -123,7 +149,6 @@ final class ViewModelTests: XCTestCase {
     }
     
     func testGivenTwoAndFourWhenDidPressSelectMultiplicationThenResultIs8() {
-        
         viewModel.didPressNumberButton(with: "2")
         viewModel.didSelectOperator(at: 2)
         viewModel.didPressNumberButton(with: "4")
@@ -132,18 +157,15 @@ final class ViewModelTests: XCTestCase {
         
         XCTAssertEqual(viewModel.result, 8)
     }
-    
+
     func testGivenFourAndTwoWhenDidPressSelectDivisionThenResultIs2() {
-        
         viewModel.didPressNumberButton(with: "4")
         viewModel.didSelectOperator(at: 3)
         viewModel.didPressNumberButton(with: "2")
-        
+    
         viewModel.didPressEqualButton()
         
         XCTAssertEqual(viewModel.result, 2)
     }
     
-
-
 } // End
